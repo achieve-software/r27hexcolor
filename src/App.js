222333687ab5fef83f93342be9a4908feb860dd2
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-24 21:55:36 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<h1>------</h1>
+<button>click for new background</button>
+<button>Copy the hex Value</button>
+
+<h4>
+  <a href="https://github.com/achieve-software" target='_blank'>Visit my Github Profile</a> 
+</h4>
+
       
     </div>
   );
